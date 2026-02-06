b1738116de8fd102a45c1a88728761999fe9241c
feruzsodiqboyev30 <feruzsodiqboyev30@gmail.com>
2026-02-06 10:19:54 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,41 @@
 import telebot
-from datetime import datetime
+from telebot import types
 
-TOKEN = "8335187940:AAFsn3H2MjbseinmVxZKCHC9aJfRyRYST4c"
+TOKEN = "TOKENINGIZNI_BU_YERGA"
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(8335187940:AAFsn3H2MjbseinmVxZKCHC9aJfRyRYST4c)
 
 schedule = {
-    "monday": "📘 Ekonometrika",
-    "tuesday": "📕 Pul Va Kredit",
-    "wednesday": "📘 Investitsiya",
-    "thursday": "📕 Banklarda bugxalteriya",
-    "friday": "📙 Makroiqtisodiyot"
+    "Dushanba": "📚 *Dushanba*\n\n📌 10:00 – 11:20\n📘 Moliyaviy tahlil (Ma)\n\n━━━━━━━━━━━━━━━━━━━━\n📌 13:30 – 14:50\n📘 Ekonometrika (Ma)",
+
+    "Seshanba": "📚 *Seshanba*\n\n📌 10:00 – 11:20\n📘 Pul va kredit 2 (Ma)",
+
+    "Chorshanba": "📚 *Chorshanba*\n\n📌 10:00 – 11:20\n📘 Investitsiya loyihalarini moliyalashtirish (Ma)",
+
+    "Payshanba": "📚 *Payshanba*\n\n📌 15:00 – 16:20\n📘 Banklarda buxgalteriya hisobi 1 (Ma)",
+
+    "Juma": "📚 *Juma*\n\n📌 13:30 – 14:50\n📘 Makroiqtisodiyot I (Ma)"
 }
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Salom! /today yoz — bugungi darslar chiqadi")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add("Dushanba", "Seshanba")
+    markup.add("Chorshanba", "Payshanba")
+    markup.add("Juma")
 
-@bot.message_handler(commands=['today'])
-def today(message):
-    day = datetime.now().strftime("%A").lower()
-    bot.reply_to(message, schedule.get(day, "Bugun dars yo‘q 🙂"))
+    bot.send_message(
+        message.chat.id,
+        "📅 Kunni tanlang:",
+        reply_markup=markup
+    )
+
+@bot.message_handler(func=lambda m: m.text in schedule)
+def show_day(message):
+    bot.send_message(
+        message.chat.id,
+        schedule[message.text],
+        parse_mode="Markdown"
+    )
 
 bot.infinity_polling()
